1ea1d237e2513938c8b068e9bffad97cebf61ecd
Daniel Spencer <dms305@hku.hk>
2020-10-20 12:28:06 +0100

    Add courseNumber attribute to Course.java

--- cit591_version_control/src/cit591_version_control/Course.java
+++ cit591_version_control/src/cit591_version_control/Course.java
@@ -2,10 +2,12 @@ package cit591_version_control;
 
 public class Course {
 	String name;
+	int courseNumber;
 	int quota;
 	
-	Course(String name, int quota){
+	Course(String name, int courseNumber, int quota){
 		this.name = name;
+		this.courseNumber = courseNumber;
 		this.quota = quota;
 	}
 }
